bcba0192f59ecf9e5ea35005d1ea68b72254c3cc
erripollo <pollone.enrico@gmail.com>
2021-05-10 12:21:12 +0200

    Blocco 4 - Sanck 2

--- blocco-4/main.js
+++ blocco-4/main.js
@@ -5,15 +5,35 @@ Nome = palla
 Peso = 10
 */
 
-var Palla = {
+/* var Palla = {
     nome : 'palla',
     peso : 10
-}
+} */
 
 /* 
 Blocco 4 - Snack 1B
 Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
 */
 
-Palla.peso = prompt('inserisci peso della palla')
-console.log(Palla);
+/* Palla.peso = Number(prompt('inserisci peso della palla'))
+console.log(Palla); */
+
+
+/*
+Blocco 4 - Sanck 2
+Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
+Calcolare perimetro e area.
+*/
+
+var TriangoloRettangolo = {
+    base : 10,
+    altezza : 15,
+}
+
+var area = TriangoloRettangolo.base * TriangoloRettangolo.altezza;
+console.log(area);
+
+var ipotenusa = Math.sqrt(Math.pow(TriangoloRettangolo.base, 2) + Math.pow(TriangoloRettangolo.altezza, 2));
+
+var perimetro = ipotenusa + TriangoloRettangolo.base + TriangoloRettangolo.altezza;
+console.log(perimetro.toFixed(2));
